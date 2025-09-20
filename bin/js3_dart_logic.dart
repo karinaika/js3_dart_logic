1ca6f05c567f840cb5e1f8039273c425363a87c3
karinaika <karinaikaindasa@gmail.com>
2025-09-20 08:48:54 +0700

    Initialize repository

---
+++ bin/js3_dart_logic.dart
@@ -0,0 +1,5 @@
+import 'package:js3_dart_logic/js3_dart_logic.dart' as js3_dart_logic;
+
+void main(List<String> arguments) {
+  print('Hello world: ${js3_dart_logic.calculate()}!');
+}
---
+++ test/js3_dart_logic_test.dart
@@ -0,0 +1,8 @@
+import 'package:js3_dart_logic/js3_dart_logic.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
