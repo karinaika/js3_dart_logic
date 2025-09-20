56831d8f2a8c3582093bfda09b07edc363b08dcb
karinaika <karinaikaindasa@gmail.com>
2025-09-20 09:17:36 +0700

    tambah input angka

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,9 @@
+import 'dart:io';
+
 void main() {
   print("Program Dart");
+  stdout.write("Masukkan angka: ");
+  int angka = int.parse(stdin.readLineSync()!);
+
+  print("Anda memasukkan angka: $angka");
 }
